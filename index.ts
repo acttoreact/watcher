6d69db7f978bc7a41bfdd32bcfc0575d067c4643
Ignacio Ferro Picón <nacho@binpar.com>
2020-04-29 16:24:37 +0200

    fix

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import initWatchers from './utils/initWatchers';
 
-initWatchers('./test/mocks/server').then((): void => {
-   // console.log('Watchers ready and listening');
-});
+initWatchers('./test/mocks/server');
+
+// eslint-disable-next-line no-console
+console.log('Watchers ready and listening');;
