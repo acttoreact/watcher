dcb633ff02c35b0daebf1d7af651f569011bd55e
Ignacio Ferro Picón <nacho@binpar.com>
2020-04-29 16:21:29 +0200

    fix

--- utils/initWatchers.ts
+++ utils/initWatchers.ts
@@ -11,13 +11,8 @@ const initWatchers = async (serverPath: string): Promise<void> => {
   if (!pathExists) {
     throw new Error(`Provided server path doesn't exist`);
   }
-<<<<<<< HEAD
   // eslint-disable-next-line no-console
   console.log('Starting watchers');
-=======
-  // console.log('Starting watchers');
->>>>>>> d3e25896e4f123289bc8321bc005521e9098426a
-
   // const apiPath = path.resolve(serverPath, 'api');
   // const modelPath = path.resolve(serverPath, 'api');
 };
