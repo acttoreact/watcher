acd3e0b8e79a093a8e1f240b045e22bae7177667
miguelBinpar <miguel@binpar.com>
2020-04-30 16:52:30 +0200

    added settings file with targetPath and runtimePath

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 import chokidar from 'chokidar';
 
+import { targetPath } from './settings';
 import initWatchers from './utils/initWatchers';
 
 export const activeWatchers: chokidar.FSWatcher[] = [];
 
-const serverPath = path.resolve(__dirname, './server');
+const serverPath = path.resolve(__dirname, targetPath);
 interface Process {
   type: 'start' | 'stop';
   callback: (watchers: chokidar.FSWatcher[]) => void;
---
+++ settings/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Default target path for watchers, should contain `api` and `model` folders
+ */
+export const targetPath = './server';
+
+/**
+ * Default runtime target path, where watcher will copy api files once validated
+ */
+export const runtimePath = './.a2r/runtime';
