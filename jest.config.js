aae41a7f6ce8fe48e59dc66749675f21fa7abb71
miguelBinpar <miguel@binpar.com>
2020-05-14 19:10:31 +0200

    updated more dist ocurrenced to bin

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   collectCoverageFrom: [
     "**/*.{ts,tsx}",
     "!**/node_modules/**",
-    "!**/dist/**",
+    "!**/bin/**",
     "!**/test/mocks/**"
   ],
   coverageReporters: ['text', 'lcov', 'json', 'html'],
